060f8d339808d833d6cfe49cd338a070a975c5a1
Debarshi Mitra <mitradebarshi17@gmail.com>
2020-09-11 23:43:46 +0530

    Add Student class and DB connection

---
+++ src/com/debarshi/web/jdbc/StudentDbUtil.java
@@ -0,0 +1,7 @@
+package com.debarshi.web.jdbc;
+
+public class StudentDbUtil {
+
+	
+	
+}
---
+++ src/com/debarshi/web/jdbc/TestServlet.java
@@ -0,0 +1,62 @@
+package com.debarshi.web.jdbc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+	// define dataSource/resource pool for resource injection
+	@Resource(name="jdbc/web_student_tracker")
+	private DataSource dataSource;
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// step 1: Setup the printwriter
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/plain");
+		
+		// step 2: connect to the database
+		Connection myConn = null;
+		Statement myStmt = null;
+		ResultSet myRs = null; 
+		
+		try {
+			myConn = dataSource.getConnection();
+			// step 3: create SQL statements
+			String sql = "select * from student";
+			myStmt = myConn.createStatement();
+			
+			// step 4: execute the SQL query
+			myRs = myStmt.executeQuery(sql); 
+			
+			// step 5: process the result set
+			while(myRs.next()) {
+				String email = myRs.getString("email");
+				out.println(email);
+			}
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+
+}
