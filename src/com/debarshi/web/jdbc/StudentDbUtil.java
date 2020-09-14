20d650ad41214a6069f92ab6d7630b4b18121ee5
Debarshi Mitra <mitradebarshi17@gmail.com>
2020-09-14 23:39:21 +0530

    Add StudentDbUtil

--- src/com/debarshi/web/jdbc/StudentDbUtil.java
+++ src/com/debarshi/web/jdbc/StudentDbUtil.java
@@ -1,7 +1,13 @@
 package com.debarshi.web.jdbc;
 
+import javax.sql.DataSource;
+
 public class StudentDbUtil {
 
+	private DataSource dataSource;
 	
+	public StudentDbUtil(DataSource theDataSource) {
+		dataSource = theDataSource;
+	}
 	
 }
