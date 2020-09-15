5140cfdd5c548ca97d27d4f0608977510c86c6ea
Debarshi Mitra <mitradebarshi17@gmail.com>
2020-09-15 23:50:32 +0530

    Update StudentDbUtil

--- src/com/debarshi/web/jdbc/StudentDbUtil.java
+++ src/com/debarshi/web/jdbc/StudentDbUtil.java
@@ -1,5 +1,11 @@
 package com.debarshi.web.jdbc;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.sql.DataSource;
 
 public class StudentDbUtil {
@@ -8,6 +14,69 @@ public class StudentDbUtil {
 	
 	public StudentDbUtil(DataSource theDataSource) {
 		dataSource = theDataSource;
+	}
+	
+	public List<Student> getStudents() throws Exception {
+		
+		List<Student> students = new ArrayList<>();
+		
+		Connection myConn = null;
+		Statement myStmt = null;
+		ResultSet myRs = null;
+		
+		try {
+			// get a connection
+			myConn = dataSource.getConnection();
+			
+			// create sql statements
+			String sql = "select * from student order by last_name";
+			myStmt = myConn.createStatement();
+			
+			// execute query
+			myRs = myStmt.executeQuery(sql);
+			
+			// process result set
+			while(myRs.next()) {
+				
+				// retrieve data from result set row
+				int id = myRs.getInt("id");
+				String firstName = myRs.getString("first_name");
+				String lastName = myRs.getString("last_name");
+				String email = myRs.getNString("email");
+				
+				// create new student object
+				Student tempStudent = new Student(id, firstName, lastName, email); 
+				
+				// add it to the list of students
+				students.add(tempStudent);
+				
+			}
+			
+			return students;
+			
+		} finally {
+			// close JDBC objects
+			close(myConn, myStmt, myRs);
+			
+		}
+	}
+
+	private void close(Connection myConn, Statement myStmt, ResultSet myRs) {
+		// TODO Auto-generated method stub
+		try {
+			if(myRs!=null) {
+				myRs.close();
+			}
+			if(myConn!=null) {
+				myRs.close();
+			}
+			if(myRs!=null) {
+				myStmt.close(); // doesn't really close it, puts back in connection pool
+			}
+			
+		} catch(Exception exc) {
+			exc.printStackTrace();
+		}
 	}
 	
 }
